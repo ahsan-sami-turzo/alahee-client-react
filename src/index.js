11ebb8c82b8c016ffc8cac9a62da73e0e38e71fd
turzo <itturzo@gmail.com>
2023-05-21 16:40:39 +0600

    react-snap added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import ReactDOM, { render } from "react-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-
+import App from "./App";
 import ScrollToTop from "./assets/ScrollToTop";
+import { persistor, store } from "./redux/store";
 
 
-import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-multi-carousel/lib/styles.css';
 
-import "./assets/social-share.css";
-import "./assets/selectImage.css";
-import "./assets/scrollToTop.css";
 import "./assets/notFoundPage.css";
+import "./assets/scrollToTop.css";
+import "./assets/selectImage.css";
+import "./assets/social-share.css";
 
 
-import './assets/styles_update/fontawesome/css/all.min.css';
 import './assets/styles_update/auth.css';
 import './assets/styles_update/checkout.css';
 import './assets/styles_update/contactUs.css';
 import './assets/styles_update/custom-accordion-styles.css';
 import './assets/styles_update/custom-styles.css';
+import './assets/styles_update/featured-category.css';
+import './assets/styles_update/fontawesome/css/all.min.css';
 import './assets/styles_update/footer.css';
 import './assets/styles_update/header.css';
 import './assets/styles_update/home-page.css';
@@ -35,24 +35,40 @@ import './assets/styles_update/registration-form.css';
 import './assets/styles_update/shopping-cart.css';
 import './assets/styles_update/social-login.css';
 import './assets/styles_update/vendor.css';
-import './assets/styles_update/featured-category.css';
 
+const rootElement = document.getElementById("root");
 
-const render = () => {
-  return ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <ScrollToTop />
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById("root")
-  );
-};
-
-render(App);
+if (rootElement.hasChildNodes()) {
+  const hydrate = () => {
+    return ReactDOM.hydrate(
+      <Provider store={store}>
+        <BrowserRouter>
+          <PersistGate persistor={persistor}>
+            <ScrollToTop />
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </Provider>,
+      document.getElementById("root")
+    );
+  };
+  hydrate(App);
+} else {
+  const render = () => {
+    return ReactDOM.render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <PersistGate persistor={persistor}>
+            <ScrollToTop />
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </Provider>,
+      document.getElementById("root")
+    );
+  };
+  render(App);
+}
 
 if (module.hot) {
   module.hot.accept("./App", () => {
